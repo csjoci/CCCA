04f55a394688ce7ab5632d58d0f27f7200396761
Jociane Costa <jocianecosta.cs@gmail.com>
2024-02-12 16:01:09 -0300

    refact: refactoring validateCpf and test

--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -1,64 +1,21 @@
 import { validateCpf } from "../src/validateCpf";
 
-test("should test if cpf is valid", function () {
-  //given
-  const cpf = "94780338085";
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(true);
-});
+test.each(["94780338085", "85222469042", "81421665000"])(
+  "should test if cpf is valid: %s",
+  function (cpf: string) {
+    //when
+    const isValid = validateCpf(cpf);
+    //then
+    expect(isValid).toBe(true);
+  }
+);
 
-test("should test if cpf is valid with one zero digit ", function () {
-  //given
-  const cpf = "85222469042";
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(true);
-});
-
-test("should test if cpf is valid with zero digits ", function () {
-  //given
-  const cpf = "81421665000";
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(true);
-});
-
-test("should test if cpf is invalid ", function () {
-  //given
-  const cpf = "422050860";
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(false);
-});
-
-test("should test if cpf is null ", function () {
-  //given
-  const cpf = null;
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(false);
-});
-
-test("should test if cpf is undefined ", function () {
-  //given
-  const cpf = undefined;
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(false);
-});
-
-test("should test if every digit is equal", function () {
-  //given
-  const cpf = "11111111111";
-  //when
-  const isValid = validateCpf(cpf);
-  //then
-  expect(isValid).toBe(false);
-});
+test.each(["422050860", null, undefined, "11111111111"])(
+  "should test if cpf is invalid: %s ",
+  function (cpf: any) {
+    //when
+    const isValid = validateCpf(cpf);
+    //then
+    expect(isValid).toBe(false);
+  }
+);
